53d487a55f0f8cb9f0e0c9c5f719cb5677708a11
Webdevk28 <117263501+Webdevk28@users.noreply.github.com>
2024-08-16 12:51:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 // ======== Respoinsive Menu ========
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "100%";
 }
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0px";
+  document.getElementById("mySidenav").style.width = "0";
 }
 
 
